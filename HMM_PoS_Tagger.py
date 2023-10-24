19572a575a6d9fa8d860ff0595912612f9fb2cda
anegda <anegdaldama@gmail.com>
2023-10-24 11:55:42 +0200

    HEADERS OF METHODS OF HMM_PoS_Tagger.py

---
+++ HMM_PoS_Tagger.py
@@ -0,0 +1,31 @@
+class HMM_PoS_Tagger:
+    def __init__(self):
+        self.trans_prob = []
+        self.emis_prob = []
+
+    def train(self, trainCorpus):
+        print("Training...")
+
+    def evaluate(self, testCorpus):
+        print("Evaluating...")
+
+    def accuracy(self, testCorpus):
+        return 0
+
+    def precision(self, testCorpus):
+        return 0
+
+    def recall(self, testCorpus):
+        return 0
+
+    def confusion_matrix(self, testCorpus):
+        print('Confusion matrix')
+
+    def predict(self, sentence):
+        print("Predicting...")
+
+    def probability(self, pair):
+        print("Calculating probability...")
+
+    def save_model(self):
+        print("Saving model...")
