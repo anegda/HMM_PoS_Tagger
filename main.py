d8b247cf922409b2dfcdb3010e7fb2db1ee7381c
anegda <anegdaldama@gmail.com>
2023-10-24 12:47:19 +0200

    PREPROCESS: CONLLU INPUT DATA

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import HMM_PoS_Tagger
+import pickle
+from conllu import parse_incr
+
+def conllu_preprocess(file):
+    data_file = open(file, "r", encoding="utf-8")
+    trainCorpus = []
+    sentence = []
+    for line in data_file:
+        line = line.split('\t')
+        if len(line) > 2:
+            if '-' not in line[0]:
+                if int(line[0]) == 1:
+                    if sentence != []:
+                        trainCorpus.append(sentence)
+                    sentence = [(line[1], line[3])]
+                else:
+                    sentence.append((line[1], line[3]))
+    return trainCorpus
+
 def main():
     print('''WELCOME TO THE HMM Part of Speech Tagger
 
@@ -12,13 +32,17 @@ def main():
 
     if int(eleccion) == 1:
         print("Training polish model")
+        trainCorpus = conllu_preprocess("./Corpus/Polish/pl_lfg-ud-train.conllu")
+        print(trainCorpus)
         main()
 
     elif int(eleccion) == 2:
         print("Training portuguese model")
+        trainCorpus = conllu_preprocess("./Corpus/Portuguese/pt_petrogold-ud-train.conllu")
+        print(trainCorpus)
         main()
 
-    elif int(eleccion) == 4:
+    elif int(eleccion) == 3:
         print("EXIT...")
         return
 
