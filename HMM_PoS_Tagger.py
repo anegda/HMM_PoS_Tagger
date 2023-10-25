cfe5b80c5be5e6c1d41827ebde862927511287f0
Warcos08 <mmerino08p@gmail.com>
2023-10-25 12:27:17 +0200

    'train' method comments in eng

--- HMM_PoS_Tagger.py
+++ HMM_PoS_Tagger.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+import os
 class HMM_PoS_Tagger:
     def __init__(self):
         self.trans_prob = {}
@@ -8,16 +9,17 @@ class HMM_PoS_Tagger:
     def train(self, trainCorpus):
         print("Training...")
 
-        # Los datos de entrada son una lista de listas de tuplas word-tag
+        # Data is a list of lists (sentences) of tuples word-tag
 
         for sentence in trainCorpus:
 
-            prev_tag = "Start"  # Para calcular las probabilidades de inicio
+            prev_tag = "Start"  # Tag for calculating the initial probabilities
             for pair in sentence:
                 word = pair[0]
                 tag = pair[1]
 
-                # Formar las matrices contando el numero de apariciones de words y de tags
+                # Build the matrices by counting the number of appearances of each word and tag
+                # Emission matrix
                 if word not in self.emis_prob.keys():
                     self.emis_prob[word] = {}
                     self.emis_prob[word][tag] = 1
@@ -27,6 +29,7 @@ class HMM_PoS_Tagger:
                     else:
                         self.emis_prob[word][tag] += 1
 
+                # Transition matrix
                 if prev_tag not in self.trans_prob.keys():
                     self.trans_prob[prev_tag] = {}
                     self.trans_prob[prev_tag][tag] = 1
@@ -44,13 +47,15 @@ class HMM_PoS_Tagger:
             else:
                 self.trans_prob[prev_tag]["Stop"] += 1
 
-        # Transformar los valores count en probabilidades de aparición
+        # Change from the count to the log probability of apparitions
+        # Emission matrix
         for word in self.emis_prob.keys():
             total_apariciones = sum(self.emis_prob[word].values())
             for tag in self.emis_prob[word].keys():
                 conteo_tag = self.emis_prob[word][tag]
                 self.emis_prob[word][tag] = np.log(conteo_tag / total_apariciones)
 
+        # Transition matrix
         for prev_tag in self.trans_prob.keys():
             total_apariciones = sum(self.trans_prob[prev_tag].values())
             for tag in self.trans_prob[prev_tag].keys():
